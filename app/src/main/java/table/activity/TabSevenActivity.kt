e7dad8a66f4038ceeef4c9cbdf22ec91181b32ed
Administrator <jz467@uowmail.edu.au>
2020-09-06 16:20:24 +0800

    ALL GOOD

--- app/src/main/java/table/activity/TabSevenActivity.kt
+++ app/src/main/java/table/activity/TabSevenActivity.kt
@@ -26,19 +26,44 @@ class TabSevenActivity : Activity() {
         super.onStart()
 
         var myApp =  this.application as MyApplication
-        val tabsix = myApp.menuListL?.get(6) as menuItem
+        val tab = myApp.menuListL?.get(6) as menuItem
 
-        var tips = tabsix.tips
-        var title = tabsix.title
-        var content = tabsix.content
-
+        var tips = tab.tips
+        var title = tab.title
+        var content = tab.content
+        var question1 = tab.getDropDownBoxByIndex(0).content
+        var question2 = tab.getDropDownBoxByIndex(1).content
+        var question3 = tab.getDropDownBoxByIndex(2).content
+        var question4 = tab.getDropDownBoxByIndex(3).content
+        var question5 = tab.getDropDownBoxByIndex(4).content
+        var question6 = tab.getDropDownBoxByIndex(5).content
+        var question7 = tab.getDropDownBoxByIndex(6).content
+        var other = tab.getDropDownBoxByIndex(7).content
 
         findViewById<TextView>(R.id.tab_title).text = title
-        findViewById<TextView>(R.id.tabsix_tips_content).text = Tool.get().functionText(this,tips)
-        findViewById<TextView>(R.id.tabsix_content).text = Tool.get().functionText(this,content)
+        findViewById<TextView>(R.id.tab_tips_content).text = Tool.get().functionText(this,tips)
+        findViewById<TextView>(R.id.tab_content).text = Tool.get().functionText(this,content)
 
-        findViewById<TextView>(R.id.tabsix_tips_content).movementMethod = LinkMovementMethod.getInstance()
-        findViewById<TextView>(R.id.tabsix_content).movementMethod = LinkMovementMethod.getInstance()
+        findViewById<TextView>(R.id.tab_tips_content).movementMethod = LinkMovementMethod.getInstance()
+        findViewById<TextView>(R.id.tab_content).movementMethod = LinkMovementMethod.getInstance()
+
+        question1=question1?.replace("\n", "")
+        question2=question2?.replace("\n", "")
+        question3=question3?.replace("\n", "")
+        question4=question4?.replace("\n", "")
+        question5=question5?.replace("\n", "")
+        question6=question6?.replace("\n", "")
+        question7=question7?.replace("\n", "")
+
+        findViewById<TextView>(R.id.question1_content).text = question1
+        findViewById<TextView>(R.id.question2_content).text = question2
+        findViewById<TextView>(R.id.question3_content).text = question3
+        findViewById<TextView>(R.id.question4_content).text = question4
+        findViewById<TextView>(R.id.question5_content).text = question5
+        findViewById<TextView>(R.id.question6_content).text = question6
+        findViewById<TextView>(R.id.question7_content).text = question7
+        findViewById<TextView>(R.id.other_content).text = Tool.get().functionText(this,other)
+        findViewById<TextView>(R.id.other_content).movementMethod = LinkMovementMethod.getInstance()
 
     }
 
